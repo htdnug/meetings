d8561e4a257d4c8ab5e696d6a925f2e3c3403580
knight0323 <jeremy@theknights.me>
2016-07-31 13:09:56 -0500

    added july 2016 meeting example

---
+++ 1607-Knockout/KnockoutExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace KnockoutExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Knockout()
+        {
+            ViewBag.Message = "A Knockout example page.";
+            return View();
+        }
+    }
+}
